1a47f82dc922f7be72c50e68100841c80331cc25
Nithya AJ <aj7nithya@gmail.com>
2025-06-12 17:37:13 +0530

    create overlay style for the gallery cards

--- client/src/components/galleryItem/galleryItem.jsx
+++ client/src/components/galleryItem/galleryItem.jsx
@@ -1,13 +1,110 @@
 import "./galleryItem.css";
+import { Link } from "react-router";
+import Button from "@mui/material/Button";
+import { RiShare2Line } from "react-icons/ri";
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import { BsThreeDots } from "react-icons/bs";
 
 const GalleryItem = ({ item }) => {
   return (
-    <div
+    <Box
       className="galleryItem"
-      style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
+      sx={{
+        display: "flex",
+        position: "relative",
+        gridRowEnd: `span ${Math.ceil(item.height / 100)}`,
+        ":hover .overlayIcons": {
+          display: "flex",
+          position: "absolute",
+          bottom: "16px",
+          right: "16px",
+          alignItems: "center",
+          gap: "8px",
+        },
+        ":hover .overlay ": {
+          display: "block",
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          backgroundColor: "rgba(0, 0, 0, 0.3)",
+          borderRadius: "16px",
+        },
+        ":hover .saveBtn": {
+          display: "block",
+          borderRadius: "24px",
+          textTransform: "none",
+          position: "absolute",
+          top: "16px",
+          right: "16px",
+        },
+      }}
     >
-      <img src={item.media} alt="" />
-    </div>
+      <Box
+        component="img"
+        src={item.media}
+        alt=""
+        loading="lazy"
+        sx={{
+          width: "100%",
+          borderRadius: "16px",
+          objectFit: "cover",
+        }}
+      />
+
+      <Link
+        to={`/pin/${item.id}`}
+        className="overlay"
+        sx={{
+          display: "none",
+        }}
+      ></Link>
+      <Button
+        className="saveBtn"
+        variant="contained"
+        color="error"
+        sx={{
+          display: "none",
+        }}
+      >
+        Save
+      </Button>
+      <Box
+        className="overlayIcons"
+        sx={{
+          display: "none",
+        }}
+      >
+        <IconButton
+          aria-label="share"
+          size="medium"
+          sx={{
+            color: "black",
+            backgroundColor: "#EBEBEB",
+            "&:hover": {
+              backgroundColor: "#E0E0E0",
+            },
+          }}
+        >
+          <RiShare2Line />
+        </IconButton>
+        <IconButton
+          aria-label="share"
+          size="medium"
+          sx={{
+            color: "black",
+            backgroundColor: "#EBEBEB",
+            "&:hover": {
+              backgroundColor: "#E0E0E0",
+            },
+          }}
+        >
+          <BsThreeDots />
+        </IconButton>
+      </Box>
+    </Box>
   );
 };
 
