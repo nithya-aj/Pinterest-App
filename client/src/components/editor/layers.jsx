51fc44e041d99df295804b13a124ae830809bd09
Nithya AJ <aj7nithya@gmail.com>
2025-08-11 16:49:57 +0530

    feat : working on the editor page

--- client/src/components/editor/layers.jsx
+++ client/src/components/editor/layers.jsx
@@ -1,20 +1,40 @@
 import Text from "../../../public/general/text.png";
+import useEditorStore from "../../utils/editorStore";
 
 const Layers = () => {
+  const { selectedLayer, setSelectedLayer, addText, canvasOptions } =
+    useEditorStore();
+
+  const handleSelectedLayer = (layer) => {
+    setSelectedLayer(layer);
+    if (layer === "text") {
+      addText();
+    }
+  };
+
   return (
     <div className="layers">
       <div className="layersTitle">
         <h3>Layers</h3>
         <p>Select a layer to edit</p>
       </div>
-      <div className="layer">
+      <div
+        onClick={() => handleSelectedLayer("text")}
+        className={`layer ${selectedLayer === "text" ? "selected" : ""}`}
+      >
         <div className="layerImg">
           <img src={Text} alt="" width={48} height={48} />
         </div>
         <span>Add Text</span>
       </div>
-      <div className="layer">
-        <div className="layerImg" style={{ backgroundColor: "teal" }}></div>
+      <div
+        onClick={() => handleSelectedLayer("canvas")}
+        className={`layer ${selectedLayer === "canvas" ? "selected" : ""}`}
+      >
+        <div
+          className="layerImg"
+          style={{ backgroundColor: canvasOptions.backgroundColor }}
+        ></div>
         <span>Canvas</span>
       </div>
     </div>
--- client/src/components/editor/workspace.jsx
+++ client/src/components/editor/workspace.jsx
@@ -1,5 +1,72 @@
-const Workspace = () => {
-  return <div className="workspace">Workspace</div>;
+import IconButton from "@mui/material/IconButton";
+import useEditorStore from "../../utils/editorStore";
+import { MdDelete } from "react-icons/md";
+import { useEffect } from "react";
+
+const Workspace = ({ previewImg }) => {
+  const { textOptions, setTextOptions, canvasOptions, setCanvasOptions } =
+    useEditorStore();
+
+  useEffect(() => {
+    if (canvasOptions.height === 0) {
+      const canvasHeight = (375 * previewImg.height) / previewImg.width;
+      setCanvasOptions({
+        ...canvasOptions,
+        height: canvasHeight,
+        orientation: canvasHeight > 375 ? "portrait" : "landscape",
+      });
+    }
+  }, [previewImg, canvasOptions, setCanvasOptions]);
+
+  return (
+    <div className="workspace">
+      <div
+        className="canvas"
+        style={{
+          height: canvasOptions.height,
+          backgroundColor: canvasOptions.backgroundColor,
+        }}
+      >
+        <img src={previewImg.url} alt="" />
+        {textOptions.text && (
+          <div
+            className="text"
+            style={{
+              left: textOptions.left,
+              top: textOptions.top,
+              fontSize: `${textOptions.fontSize}px`,
+            }}
+          >
+            <input
+              type="text"
+              value={textOptions.text}
+              onChange={(e) =>
+                setTextOptions({ ...textOptions, text: e.target.value })
+              }
+              style={{ color: textOptions.color }}
+            />
+            <IconButton
+              aria-label="share"
+              size="medium"
+              sx={{
+                color: "black",
+                backgroundColor: "#EBEBEB",
+                position: "absolute",
+                top: "-43px",
+                right: "6px",
+                "&:hover": {
+                  backgroundColor: "#E0E0E0",
+                },
+              }}
+              onClick={() => setTextOptions({ ...textOptions, text: "" })}
+            >
+              <MdDelete />
+            </IconButton>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Workspace;
